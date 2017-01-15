d965deaf2b26de6c66edb45a1ae9dd289948f9a5
Matt Warman <mkwarman@gmail.com>
2017-01-14 20:47:35 -0500

    trying to get command to work

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ slapp.message('.*', ['direct_mention', 'direct_message'], (msg) => {
 })
 
 slapp.command('bible', (msg) => {
-  msg.say('Received slash command for Bible');
+  msg.respond('Received slash command for Bible');
 })
 
 // attach Slapp to express server
