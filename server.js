2c8a2a74e038c1435ae1bd6632192a9699562e18
mkwarman <matthew_k_warman@homedepot.com>
2017-01-17 09:28:51 -0500

    fixed exec function

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ function parseVerseData(text) {
   var booksArray = verse.match(/[A-Za-z]+/g);
 
   // Get verses, but only the first verse for each book
-  var firstVerseArray = verse.exec(/[A-Za-z]+\+?(\d+:\d+)/g)[1];
+  var firstVerseArray = /[A-Za-z]+\+?(\d+:\d+)/g.exec(verse)[1];
   // These should return the same thing in capture group 1:
   // /[A-Za-z]+\+?(\d+:\d+)/g
   // /(\d+:\d+)(-\d+)?((\+\d+:\d+(-\d+)?)+)?/g
